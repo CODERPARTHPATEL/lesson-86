87dc01f12c44e485dfc578cb8c824447861eee27
CODERPARTHPATEL <nskp.patel09@gmail.com>
2026-06-26 19:46:08 +0100

    lesson 86

---
+++ quartile.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#import dataset
+data = pd.read_csv('Titanic Dataset.csv')
+
+data.head(5)
+
+data.isnull().sum()
+
+#null values present
+#quartiles of age
+
+age_q1 = np.quantile(data['Age'],0.25)
+age_q2 = np.quantile(data['Age'],0.50)
+age_q3 = np.quantile(data['Age'],0.75)
+print('Age Quartiles')
+
+print('Q1 -', age_q1)
+print('Q2 -', age_q2)
+print('Q3 -', age_q3)
+
+IQR_age = age_q3 - age_q1
+print('Interquartile Range :',IQR_age)
+
+#plot histogram
+plt.hist(data['Age'])
+plt.ylabel('count of passengers')
+plt.xlabel('Age')
+plt.show()
+
+#quartiles for feature fare
+Fare_q1 = np.quantile(data['Fare'],0.25)
+Fare_q2 = np.quantile(data['Fare'],0.50)
+Fare_q3 = np.quantile(data['Fare'],0.75)
+
+print('Fare Quartiles -')
+print('Q1 -',Fare_q1)
+print('Q2 -',Fare_q2)
+print('Q3 -',Fare_q3)
+
+IQR_fare = Fare_q3 - Fare_q1
+print('Interquartile Range:', IQR_fare)
+
+###plot histogram 2
+bins = np.arange(0,250,20)
+plt.hist(data['Fare'],bins=np.arange(1,250,20))
+plt.ylabel('count of passengers')
+plt.xlabel('Fare')
+plt.xticks(bins)
+
+plt.show()
